a612be4a29caabd1aa2f4b227511eb7ea64e6e5a
Scott Bedard <scottbedard90@gmail.com>
2024-05-19 17:02:06 -0500

    scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -4,5 +4,7 @@ import Maze from '@/components/Maze'
 export default function App() {
   const [screen] = useWindowSize()
 
-  return <Maze size={screen()} />
+  return <div class="flex h-screen items-center justify-center w-screen">
+    <Maze screen={screen()} />
+  </div>
 }
--- src/components/Maze.tsx
+++ src/components/Maze.tsx
@@ -1,9 +1,18 @@
 import { Vec } from '@/types'
 
 type Props = {
-  size: Vec<2>
+  screen: Vec<2>
 }
 
 export default function Maze(props: Props) {
-  return <div>maze: {JSON.stringify(props)}</div>
+  const dimensions = () => [
+    Math.floor(props.screen[0] / 20),
+    Math.floor(props.screen[1] / 20),
+  ]
+
+  const canvas = <canvas class="border-4 border-dashed border-green-500 h-full w-full" />
+
+  console.log({ canvas })
+
+  return canvas
 }
