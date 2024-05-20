9e95f512849e41c8a6bd1210e8ea8f4f1a34f866
Scott Bedard <scottbedard90@gmail.com>
2024-05-19 19:33:41 -0500

    wip

--- src/components/Maze.tsx
+++ src/components/Maze.tsx
@@ -1,18 +1,35 @@
+import { onMount } from 'solid-js'
 import { Vec } from '@/types'
 
 type Props = {
   screen: Vec<2>
 }
 
+let canvas: HTMLCanvasElement
+
 export default function Maze(props: Props) {
+
   const dimensions = () => [
     Math.floor(props.screen[0] / 20),
     Math.floor(props.screen[1] / 20),
   ]
 
-  const canvas = <canvas class="border-4 border-dashed border-green-500 h-full w-full" />
+  onMount(() => {
+    const ctx = canvas.getContext('2d');
 
-  console.log({ canvas })
+    if (!ctx) {
+      return
+    }
 
-  return canvas
+    // ctx.fillRect(25, 25, 100, 100);
+    // ctx.clearRect(45, 45, 60, 60);
+    // ctx.strokeRect(50, 50, 50, 50);
+  })
+
+  return <div>
+    <canvas
+      class="border-4 border-dashed border-green-500 h-full w-full"
+      ref={canvas}
+    />
+  </div>
 }
