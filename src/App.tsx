5dc1e6ecef1758175bfa359c20c9dd9a159c4428
Scott Bedard <scottbedard90@gmail.com>
2024-05-19 21:41:43 -0500

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+import { createSignal } from 'solid-js'
 import { range } from 'lodash-es'
+import { Vec } from '@/types'
 import { useWindowSize } from '@/behaviors'
 import Maze from '@/components/Maze'
 
 export default function App() {
+  const [mouse, setMouse] = createSignal<Vec<2>>([0, 0])
   const [screen] = useWindowSize()
+  const [cols, setCols] = createSignal(5)
+  const [rows, setRows] = createSignal(5)
 
-  const rows = 10
-  const cols = 10
+  console.log(cols(), rows())
 
-  const grid = range(0, rows).map(() => range(0, cols))
+  const grid = () => range(0, cols()).map(() => range(0, rows()))
 
-  console.log({ grid })
+  const onMouseMove = (evt: MouseEvent) => setMouse([evt.clientX, evt.clientY])
+
   return <div>
     <Maze
-      grid={grid}
+      grid={grid()}
+      onMouseMove={onMouseMove}
       screen={screen()} />
   </div>
 }
--- src/components/Maze.tsx
+++ src/components/Maze.tsx
@@ -1,49 +1,60 @@
 import { onMount } from 'solid-js'
-import { random, throttle } from 'lodash-es'
 import { useRequestAnimationFrame } from '@/behaviors'
 import { Vec } from '@/types'
 
 type Props = {
   grid: unknown[][]
+  onMouseMove: (evt: MouseEvent) => void
   screen: Vec<2>
 }
 
 let canvas: HTMLCanvasElement
 
 export default function Maze(props: Props) {
-  let ctx: CanvasRenderingContext2D
+  let ctx: CanvasRenderingContext2D | null = null
 
-  const render = throttle(() => {
+  const render = () => {
+    if (!ctx) {
+      return
+    }
+  
+    const stroke = 5
+
     const [width, height]: Vec<2> = [
       props.screen[0] / props.grid.length,
       props.screen[1] / props.grid[0].length,
     ]
 
-    props.grid.forEach((row, i) => {
-      row.forEach((_col, j) => {
+    for (let col = 0; col < props.grid.length; col++) {
+      for (let row = 0; row < props.grid[col].length; row++) {
+        const x = col * width + col
+        const y = row * height + row
         ctx.beginPath()
-        ctx.strokeStyle = 'red'
-        ctx.fillStyle = `rgba(${random(255)}, ${random(255)}, ${random(255)})`
-        ctx.rect(i * width + i, j * height + j, width, height)
+        ctx.rect(x, y, width - stroke, height - stroke)
+        ctx.strokeStyle = 'white'
+        ctx.fillStyle = '#cbd5e1'
         ctx.fill()
-        ctx.strokeRect(i * width + i, j * height + j, width, height)
-      })
-    })
-  }, 1000)
-
-  const onMousemove = () => {
-    console.log('mousemove')
+      }
+    }
   }
 
-  onMount(() => ctx = canvas.getContext('2d')!)
+  onMount(() => {
+    const _ctx = canvas.getContext('2d')
+
+    if (!_ctx) {
+      throw new Error('Could not get 2d canvas context')
+    }
+  
+    ctx = _ctx
+  })
 
   useRequestAnimationFrame(render, { autostart: true })
 
   return <canvas
     class="h-screen w-screen"
-    ref={canvas}
     height={props.screen[1]}
+    onMouseMove={props.onMouseMove}
+    ref={canvas}
     width={props.screen[0]}
-    onMouseMove={onMousemove}
   />
 }
